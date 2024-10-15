8353e23ad76bd1494ab8c633a7e9f08d7de5cec3
Mike Gallay <mikegallay@gmail.com>
2024-10-15 18:51:11 -0400

    map drawer

--- app/components/UploadForm.tsx
+++ app/components/UploadForm.tsx
@@ -6,10 +6,55 @@ import MySelect from '@/app/components/ui/Select';
 import MyCheckbox from '@/app/components/ui/Checkbox';
 import MyTextarea from '@/app/components/ui/Textarea';
 import MyLocationSelector from '@/app/components/ui/LocationSelector';
+import {
+  Drawer,
+  DrawerContent,
+  DrawerTrigger, DrawerHeader, DrawerTitle, DrawerDescription,DrawerClose
+} from "@/components/ui/drawer"
 import { Button } from "@/components/ui/button";
 import { raceOptions, jewishOptions, observanceLevel, kiddushFrequency, influenceLevels } from '@/app/data/uploadFormData';
-
 import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+
+function mapDrawer(register: any, setValue: any) {
+
+  const [location, setLocation] = useState<number[] | null>(null);
+  register("specific_location", { required: false })
+  function onClick() {
+    console.log('make selection');
+    const arr = [Math.random()*120 - 60, Math.random()*120 - 60]
+    setLocation(arr)
+    setValue("specific_location", arr)
+  }
+
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+        <Button variant="outline" className="justify-start">{location ? <>{'lat: ' + location[0] + ', lng: ' + location[1]} </> : <>Open Map Selector</>}</Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Can you be more specific?</DrawerTitle>
+          <DrawerDescription>Please be as specific as possible to better represent your location. You don't have to share your exact location. An approximation is fine. Drop a pin and the hit the button to make your selection.</DrawerDescription>
+        </DrawerHeader>
+        <div className="w-full h-96 bg-gray-200 flex items-center justify-center">
+          <span className="text-gray-500">Map will be displayed here</span>
+        </div>
+        <div className="p-3">
+          <div className="flex justify-center">
+            <DrawerClose asChild>
+              <Button
+                size="lg"
+                className="w-full"
+                onClick={() => onClick()}
+              >Make Selection</Button>
+            </DrawerClose>
+          </div>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  )
+}
 
 export default function UploadForm() {
   const { register, handleSubmit, setValue, formState: { errors } } = useForm();
@@ -41,11 +86,21 @@ export default function UploadForm() {
 
         <MyInput label="Email" id="email" type="email" formProps={{register, errors}} />
 
-        <MyLocationSelector label="Where do you live?" id="you_from" formProps={{register, errors, setValue}} className="w-auto lg:w-1/2" description="This location will not be exact. " required/>
+        <div className="flex gap-6 flex-col lg:flex-row">
+          <MyLocationSelector label="Where do you live?" id="you_from" formProps={{register, errors, setValue}} className="w-auto lg:w-1/2" description="This location will not be exact." />
+
+          <div className="w-3/4 flex flex-col gap-2">
+            <p className="text-sm">
+              Provide a more specific location (Optional)
+            </p>
+            {mapDrawer(register,setValue)}
+            <p className='text-gray-500 font-medium text-xs -mt-2 italic pt-1'>To better represent you on the map.</p>
+          </div>
+        </div>
 
         <MyInput label="Upload Your Kiddush Audio (optional)" id="file" type="file" description="No file to share? That's ok! We'd still love to learn more about you and where you are from." formProps={{register, errors}}/>
 
-        <MyTextarea label="Share your favorite memory about this Kiddush." id="shabbat_memory" description="If you don't upload any audio, use this space to share any Shabbat memory." formProps={{register, errors}}/>
+        <MyTextarea label="Share your favorite memory about this Kiddush. (optional)" id="shabbat_memory" description="If you don't upload any audio, use this space to share any Shabbat memory." formProps={{register, errors}}/>
 
         <h2 className="font-bold border-b-2 border-solid border-gray-700 dark:border-gray-400">Tell Us About Yourself</h2>
 
@@ -70,21 +125,21 @@ export default function UploadForm() {
         <h2 className="font-bold border-b-2 border-solid border-gray-700 dark:border-gray-400">Tell Us About Your Family</h2>
    
         <div className="flex gap-6 flex-col lg:flex-row">
-          <MyLocationSelector label="Where is Your Mother From?" id="mother_from" formProps={{register, errors, setValue}} className="w-auto lg:w-1/2" description="This location will not be exact." required/>
+          <MyLocationSelector label="Where is Your Mother From?" id="mother_from" formProps={{register, errors, setValue}} className="w-auto lg:w-1/2" description="This location will not be exact." />
 
-          <MyLocationSelector label="Where is Your Father From?" id="father_from" formProps={{register, errors, setValue}} className="w-auto lg:w-1/2" description="This location will not be exact." required/>
+          <MyLocationSelector label="Where is Your Father From?" id="father_from" formProps={{register, errors, setValue}} className="w-auto lg:w-1/2" description="This location will not be exact." />
         </div>
 
         <div className="flex gap-6 flex-col lg:flex-row">
-          <MyLocationSelector label="Where is Your Maternal Grandmother From?" id="maternal_gmother_from" formProps={{register, errors, setValue}} className="w-auto lg:w-1/2" description="This location will not be exact." required/>
+          <MyLocationSelector label="Where is Your Maternal Grandmother From?" id="maternal_gmother_from" formProps={{register, errors, setValue}} className="w-auto lg:w-1/2" description="This location will not be exact." />
 
-          <MyLocationSelector label="Where is Your Maternal Grandfather From?" id="maternal_gfather_from" formProps={{register, errors, setValue}} className="w-auto lg:w-1/2" description="This location will not be exact." required/>
+          <MyLocationSelector label="Where is Your Maternal Grandfather From?" id="maternal_gfather_from" formProps={{register, errors, setValue}} className="w-auto lg:w-1/2" description="This location will not be exact." />
         </div>
 
         <div className="flex gap-6 flex-col lg:flex-row">
-          <MyLocationSelector label="Where is Your Paternal Grandmother From?" id="paternal_gmother_from" formProps={{register, errors, setValue}} className="w-auto lg:w-1/2" description="This location will not be exact." required/>
+          <MyLocationSelector label="Where is Your Paternal Grandmother From?" id="paternal_gmother_from" formProps={{register, errors, setValue}} className="w-auto lg:w-1/2" description="This location will not be exact." />
 
-          <MyLocationSelector label="Where is Your Paternal Grandmother From?" id="paternal_gfather_from" formProps={{register, errors, setValue}} className="w-auto lg:w-1/2" description="This location will not be exact." required/>
+          <MyLocationSelector label="Where is Your Paternal Grandmother From?" id="paternal_gfather_from" formProps={{register, errors, setValue}} className="w-auto lg:w-1/2" description="This location will not be exact." />
         </div>
 
         <MyCheckbox label="I'm ok with my audio being used in social media, with the understanding that my identity and other personal information will not be shared." id="ok_with_audio" formProps={{register, errors, setValue}} defaultChecked={true}/>
--- app/components/ui/LocationSelector.tsx
+++ app/components/ui/LocationSelector.tsx
@@ -41,7 +41,7 @@ type locationDataProps = {
 
 const locations: locationDataProps[] = locationList;
 
-const pin = <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 3.5C6 2.67157 6.67157 2 7.5 2C8.32843 2 9 2.67157 9 3.5C9 4.32843 8.32843 5 7.5 5C6.67157 5 6 4.32843 6 3.5ZM8 5.94999C9.14112 5.71836 10 4.70948 10 3.5C10 2.11929 8.88071 1 7.5 1C6.11929 1 5 2.11929 5 3.5C5 4.70948 5.85888 5.71836 7 5.94999V13.5C7 13.7761 7.22386 14 7.5 14C7.77614 14 8 13.7761 8 13.5V5.94999Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+const pin = <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 3.5C6 2.67157 6.67157 2 7.5 2C8.32843 2 9 2.67157 9 3.5C9 4.32843 8.32843 5 7.5 5C6.67157 5 6 4.32843 6 3.5ZM8 5.94999C9.14112 5.71836 10 4.70948 10 3.5C10 2.11929 8.88071 1 7.5 1C6.11929 1 5 2.11929 5 3.5C5 4.70948 5.85888 5.71836 7 5.94999V13.5C7 13.7761 7.22386 14 7.5 14C7.77614 14 8 13.7761 8 13.5V5.94999Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
 
 const MyLocationSelector = (
     ({ label, id, description, onChange, options, formProps, ...props }: LocationProps) => {
@@ -63,18 +63,18 @@ const MyLocationSelector = (
           </p>
           <Popover open={open} onOpenChange={setOpen}>
             <PopoverTrigger asChild >
-              <Button variant="outline" className="w-full justify-start">
+              <Button variant="outline" className="w-auto justify-start">
                 {selectedStatus ? <>{selectedStatus.label}</> : <>{pin} Select Location</>}
               </Button>
             </PopoverTrigger>
             <PopoverContent className="w-[400px] p-0" align="start">
               <StatusList setOpen={setOpen} setSelectedStatus={setSelectedStatus} id={id} formProps={formProps} />
             </PopoverContent>
-            {formProps.errors[id]?.message ? 
-              <span className='text-rose-700 text-sm italic -mt-2'>{String(formProps.errors[id]?.message)}</span> :
-              description && <span className='text-gray-500 font-medium text-xs -mt-2 italic'>{String(description)}</span>
-            }
           </Popover>
+          {formProps.errors[id]?.message ? 
+            <span className='text-rose-700 text-sm italic -mt-2'>{String(formProps.errors[id]?.message)}</span> :
+            description && <span className='text-gray-500 font-medium text-xs -mt-2 italic pt-1'>{String(description)}</span>
+          }
         </div>
       )
     }
@@ -86,7 +86,7 @@ const MyLocationSelector = (
           </p>
         <Drawer open={open} onOpenChange={setOpen}>
           <DrawerTrigger asChild>
-            <Button variant="outline" className="w-full  justify-start">
+            <Button variant="outline" className="w-auto  justify-start">
               {selectedStatus ? <>{selectedStatus.label}</> : <>{pin} Select Location</>}
             </Button>
           </DrawerTrigger>
@@ -95,11 +95,11 @@ const MyLocationSelector = (
               <StatusList setOpen={setOpen} setSelectedStatus={setSelectedStatus} id={id} formProps={formProps} />
             </div>
           </DrawerContent>
-          {formProps.errors[id]?.message ? 
-            <span className='text-rose-700 text-sm italic -mt-2'>{String(formProps.errors[id]?.message)}</span> :
-            description && <span className='text-gray-500 font-medium text-xs -mt-2 italic'>{String(description)}</span>
-          }
         </Drawer>
+        {formProps.errors[id]?.message ? 
+          <span className='text-rose-700 text-sm italic -mt-2'>{String(formProps.errors[id]?.message)}</span> :
+          description && <span className='text-gray-500 font-medium text-xs -mt-2 italic pt-1'>{String(description)}</span>
+        }
       </div>
     )
   }
