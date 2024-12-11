4d4f1e91daf92f8ade7b46708b85ab6a437386fb
Mike Gallay <mikegallay@gmail.com>
2024-12-11 15:25:36 -0500

    clean up media stream

--- app/components/AudioRecorder.tsx
+++ app/components/AudioRecorder.tsx
@@ -10,15 +10,13 @@ const AudioRecorder = ({ onComplete, onRecording, preRecorded = null, preData = 
     const [audioData, setAudioData] = useState<Blob | null>(preData);
     // const [isMicAccessible, setIsMicAccessible] = useState(false);
     const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(null);
+    const [mediaStream, setMediaStream] = useState<MediaStream | null>(null);
     const audioChunksRef = useRef<Blob[]>([]); // Use a ref to avoid async state issues.
     
     // Clean up media recorder on unmount
     useEffect(() => {
         return () => {
-            if (mediaRecorder) {
-                audioChunksRef.current = []
-                mediaRecorder.stop();
-            }
+          stopRecording()
         }
     }, [mediaRecorder]);
 
@@ -26,6 +24,7 @@ const AudioRecorder = ({ onComplete, onRecording, preRecorded = null, preData = 
 
       try {
         const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        setMediaStream(stream);
         console.log("Stream tracks:", stream.getTracks());
   
         const mimeType = MediaRecorder.isTypeSupported("audio/webm;codecs=opus")
@@ -85,6 +84,13 @@ const AudioRecorder = ({ onComplete, onRecording, preRecorded = null, preData = 
         setIsRecording(false);
         onRecording(false);
         console.log("Recording stopped.");
+        audioChunksRef.current = []
+    }
+      if (mediaStream) {
+        // This will trigger the `onstop` event for the MediaRecorder
+        const tracks = mediaStream.getTracks();
+        tracks.forEach((track) => track.stop());
+        setMediaStream(null);
       }
     };
 
